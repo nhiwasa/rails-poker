09210ece8843d1334d4f44cc08b1fbdee0e91f58
日和佐 登 <nhiwasa@netprotections.co.jp>
2019-04-03 11:31:04 +0900

    commit

--- app/apis/api/root.rb
+++ app/apis/api/root.rb
@@ -3,8 +3,7 @@ module API
   class Root < Grape::API
     mount API::Ver1::Root
 
-    route :any, '*path' do
-      error! I18n.t('grape.errors.not_found'), 404
-    end
+
+    
   end
 end
--- app/apis/api/ver1/judge.rb
+++ app/apis/api/ver1/judge.rb
@@ -9,8 +9,8 @@ module API
       # /api/v1/judge/
       post '/judge' do
         hands = params[:cards]
-        a = []
-        b = []
+        result = []
+        roles = []
         hands.each do |hand|
           card = hand.split
           card = CardForm.new(card)
@@ -19,62 +19,27 @@ module API
           end
 
           if card.errors.present?
-            a << {card: "#{hand}", msg: "#{card.errors[0]}"}
-            b << card.role
+            result << {card: "#{hand}", msg: "#{card.errors[0]}"}
+            roles << card.role
           else
-            a << {card: "#{hand}", hand: "#{card.role}" , best: ""}
+            result << {card: "#{hand}", hand: "#{card.role}" , best: ""}
 
-            b << card.role
+            roles << card.role
           end
 
 
         end
 
-        scores = []
-        b.each do |c|
-          if  c == "ストレートフラッシュ"
-            scores << 9
-          elsif  c == "フォーカード"
-            scores << 8
-          elsif c == "フルハウス"
-            scores << 7
-          elsif c == "フラッシュ"
-            scores << 6
-          elsif c == "ストレート"
-            scores << 5
-          elsif c == "スリーカード"
-            scores << 4
-          elsif c == "ツーペア"
-            scores << 3
-          elsif c == "ワンペア"
-            scores << 2
-          elsif c == "ハイカード"
-            scores << 1
-          else
-            scores << 0
-          end
+        roles = StrengthJudgement.new(roles)
+        roles.strength_judgement
 
+        results = []
 
+        result.zip(roles.best).each do |x, y|
+          results << x.update(y)
         end
 
-        best = []
-
-        scores.each do |score|
-          if score == scores.max
-            best << {best: true}
-          else
-            best << {best: false}
-          end
-
-        end
-
-        result = []
-
-        a.zip(best).each do |x, y|
-          result << x.update(y)
-        end
-
-        reply = [result: result]
+        reply = [result: results]
 
 
 
